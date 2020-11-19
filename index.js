1867748fb3090352c00bdbb17d2c4ec5866b9269
Sagar Saha <59755509+SagarSaha01@users.noreply.github.com>
2020-11-19 18:52:34 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("%cSTOP Right Here!!! ❌💀", "color:red; font-size:50px");
+
+var style = 'color: tomato; -webkit-text-stroke: 1px black; font-size:30px;';
+console.log('%cYou are here because you want to hack my website. Now thats illegal.', style)
+
+var style1 = 'color: orange; font-size:15px;';
+console.log('%cIf you do not know what you are doing or someone told you to copy and paste something here, then you are getting hacked and it is recommend that you close this console and go back to safety.', style1)
+
+var style2 = 'color: green; font-size:20px;';
+console.log('%cIf you know exactly what you are doing, then you should come join and work with me to solve various hackathons and even win prizes!!! ✅✅', style2)
+
+var style3 = 'color: cyan; font-size:20px;';
+console.log('%cELSE GO BACK TO SAFETY ☠ ☠ ☠', style3)
+
