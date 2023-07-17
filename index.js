b7b11e51e28afc6c9e99824e2e97be222c5ae96d
Sagar Saha <sagarsaha2004@gmail.com>
2023-07-17 02:10:22 -0400

    redesign website 2023

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-console.log("%cSTOP Right Here!!! ❌💀", "color:red; font-size:50px");
-
-const style = 'color: tomato; -webkit-text-stroke: 1px black; font-size:30px;';
-console.log('%cYou are here because you want to hack my website. Now thats illegal.', style)
-
-const style1 = 'color: orange; font-size:15px;';
-console.log('%cIf you do not know what you are doing or someone told you to copy and paste something here, then you are getting hacked and it is recommend that you close this console and go back to safety.', style1)
-
-const style2 = 'color: green; font-size:20px;';
-console.log('%cIf you know exactly what you are doing, then you should come join and work with me to solve various hackathons and even win prizes!!! ✅✅', style2)
-
-const style3 = 'color: cyan; font-size:20px;';
-console.log('%cELSE GO BACK TO SAFETY ☠ ☠ ☠', style3)
+let randomNumber = Math.floor((Math.random() * 7) + 1);
+let image = "images/headers_pictures/sagar" + randomNumber + ".png";
+document.getElementById("header_image").src = image;
